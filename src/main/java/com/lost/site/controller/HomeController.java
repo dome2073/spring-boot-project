c76eb9145a7a040f93ce9878f6a8505e56a2dcf5
정대윤 <wlgns2073@gmail.com>
2020-04-07 16:57:34 +0900

    프로젝트 생성 및 기본적인 세팅 완료 ;

---
+++ src/main/java/com/lost/site/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.lost.site.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/home")
+	@ResponseBody
+	public String showMain() {
+		return "메인화면";
+	}
+}
