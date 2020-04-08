8e143bb6405c27cf48c6b88aea2b068bf630cfe5
정대윤 <wlgns2073@gmail.com>
2020-04-08 19:51:58 +0900

    ArticleImpl 삭제, Mabatis로 MariaDB 데이터 Select

--- src/main/java/com/lost/site/dao/ArticleDao.java
+++ src/main/java/com/lost/site/dao/ArticleDao.java
@@ -2,8 +2,12 @@ package com.lost.site.dao;
 
 import java.util.List;
 
-import com.lost.site.dto.Article;
+import org.apache.ibatis.annotations.Mapper;
 
+
+import com.lost.site.dto.Article;
+ 
+@Mapper //마이바티스 맵핑
 public interface ArticleDao {
 	//서비스에서 했던 일을 Dao에서
 	public List<Article> getList();
--- src/main/java/com/lost/site/service/ArticleServiceImpl.java
+++ src/main/java/com/lost/site/service/ArticleServiceImpl.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import com.lost.site.dao.ArticleDao;
@@ -17,25 +16,8 @@ public class ArticleServiceImpl implements ArticleService {
 	ArticleDao articleDao;
 	
 	public List<Article> getList(){
-		
-	
-	
-// Service는 데이터 관련해서 모두 Dao에게 위임
-		
-//		가 데이터 넣기
-		
-		Article article1 = new Article(1, "2020-04-07 21:22:01", "제목1", "내용1");
-		Article article2 = new Article(2, "2020-04-07 21:22:01", "제목2", "내용2");
-		Article article3 = new Article(3, "2020-04-07 21:22:01", "제목3", "내용3");
-		
-		
-		List<Article> list = new ArrayList<>();
-		
-		list.add(article3);
-		list.add(article2);
-		list.add(article1);
-		
-		return list;
+					
+		return articleDao.getList();
 		
 	}
 }
